d76f04f49b94ee35db573385bee5dfbd081395f1
deepak <deepaksharma64349.a@gmail.com>
2025-08-30 11:35:18 +0530

    add new branch

--- lib/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-abstract class AppRoutes {
-  static const SPLASH = '/splash';
-  static const LOGIN = '/login';
-  static const SIGNUP = '/signup';
-  static const TASK_LIST = '/tasks';
-  static const ADD_TASK = '/add-task';
-  static const EDIT_TASK = '/edit-task';
-}
